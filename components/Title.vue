a3653f9d55236e0b2aafc72da7dee834842c9cf7
Geremias Corrêa <gere.c@hotmail.com>
2021-06-14 21:28:50 -0300

    Criação de componente de título para aplicação.

---
+++ components/Title.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="uk-flex-1">
+		<h1 class="uk-text-center uk-flex-1" style="font-size:30px;padding-top:20px;font-weight: 400; color:#00737f">
+			{{ title }}
+		</h1>
+	</div>
+</template>
+
+<script>
+
+export default {
+	props: {
+		title: {
+			type: String,
+			default: ''
+		}
+	},
+	data: () => ({
+	}),
+	mounted () {
+	},
+	methods: {
+	}
+}
+</script>
+<style lang="scss">
+</style>
