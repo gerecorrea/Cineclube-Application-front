993d11b80853c1dcea39c83ca38afb792889df7f
Geremias Corrêa <gere.c@hotmail.com>
2021-06-15 21:40:46 -0300

    Adição de serviço e listagem geral de person (estrelas).

--- plugins/Cineclube_services.js
+++ plugins/Cineclube_services.js
@@ -2,10 +2,12 @@ import UserService from '@/services/userService'
 import TenantService from '@/services/tenantService'
 import LoginService from '@/services/loginService'
 import movieService from '@/services/movieService'
+import personService from '@/services/personService'
 
 export default ({ app, store }) => {
 	UserService.setClient(app.$axios)
 	TenantService.setClient(app.$axios)
 	LoginService.setClient(app.$axios)
 	movieService.setClient(app.$axios)
+	personService.setClient(app.$axios)
 }
