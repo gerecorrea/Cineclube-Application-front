01a5f2423c293a561b002a09d73d1c9c8e398bcc
gerecorrea <gere.c@hotmail.com>
2021-07-06 23:57:32 -0300

    Refatoração rápida sidebar menu, para evitar bugs.

--- components/navigation/sidebar_menu.js
+++ components/navigation/sidebar_menu.js
@@ -4,7 +4,7 @@ const { uniqueID } = scHelpers;
 export const menuEntries = [
 	{
 		id: uniqueID(),
-		title: "Dashbaord",
+		title: "Dashboard",
 		icon: "mdi mdi-view-dashboard md-color-grey-900",
 		page: "/dashboard"
 	},
@@ -29,31 +29,26 @@ export const menuEntries = [
 	{
 		id: uniqueID(),
 		title: "Top 10",
-		page: "/movies/",
 		icon: "mdi mdi-certificate md-color-red-700",
 	},
 	{
 		id: uniqueID(),
 		title: "My ratings",
-		page: "/movies/",
 		icon: "mdi mdi-script md-color-blue-800"
 	},
 	{
 		id: uniqueID(),
 		title: "Watchlist",
-		page: "/movies/",
 		icon: "mdi mdi-bookmark-plus md-color-yellow-800"
 	},
 	{
 		id: uniqueID(),
 		title: "Filmes Favoritos",
-		page: "/movies/",
 		icon: "mdi mdi-heart-half-full md-color-red-700"
 	},
 	{
 		id: uniqueID(),
 		title: "Estrelas Favoritas",
-		page: "/movies/",
 		icon: "mdi mdi-account-star md-color-green-600"
 	},
 	{
@@ -67,7 +62,6 @@ export const menuEntries = [
 			{
 				id: uniqueID(),
 				title: "Listar todos",
-				page: "/movies/listAll",
 				icon: "mdi mdi-movie",
 			},
 		]
