947c7ab001ee1c87d857263efb37589f22866272
gerecorrea <gere.c@hotmail.com>
2021-07-08 23:19:10 -0300

    Correção tipos colunas numéricas e início de aplicação de filtros (não funcional).

--- pages/movies/all.vue
+++ pages/movies/all.vue
@@ -12,13 +12,22 @@
 			<ScCard>
 				<ScCardBody>
 					<div>
-						<div data-uk-margin class="uk-text-right">
-							<nuxt-link :to=" '/movies/'">
-								<button class="sc-button " data-uk-tooltip="Cadastrar novo filme">
-									<img src="~/assets/img/movie-add.png" width="35px" alt="">
-									Inserir filme
-								</button>
-							</nuxt-link>
+						<div data-uk-margin>
+							<button 
+								class="sc-button uk-text-left mdi mdi-filter-outline md-color-blue-grey-600"
+								data-uk-tooltip="Selecionar filtros para a listagem"
+								@click="dialogFilter = true"	
+							>
+								Filtros
+							</button>
+							<button style="float: right"
+								class="sc-button uk-text-right"
+								data-uk-tooltip="Cadastrar novo filme"
+								@click="redirectPage('/movies/')"
+							>
+								<img src="~/assets/img/movie-add.png" width="35px" alt="">
+								Inserir filme
+							</button>
 						</div>
 					</div>
 					<fieldset class="uk-fieldset md-bg-blue-grey-50 sc-padding uk-margin-top">
@@ -71,6 +80,136 @@
 				</ScCardBody>
 			</ScCard>
 		</div>
+		<v-dialog
+			v-model="dialogFilter"
+			width="50%"
+			persistent
+			scrollable
+			style="z-index: 1500"
+		>
+			<v-card>
+				<v-card-title class="uk-modal-title">
+					Selecionar filtros
+				</v-card-title>
+				<v-card-text class="text--primary">
+					<div
+						class="md-color-blue-grey-100"
+					>
+						<div class="uk-flex uk-flex-bottom uk-grid" data-uk-grid>
+							<div class="uk-width-1-1@m">
+								<v-text-field
+									v-model="filterData.title"
+									hint="Digite o nome do filme"
+									label="Nome do filme"
+								></v-text-field>
+							</div>
+							<div class="uk-width-1-2@m" style="vertical-align: middle">
+								<v-text-field
+									v-model="filterData.country"
+									hint="Digite o nome do país"
+									label="País"
+								></v-text-field>
+							</div>
+							<div class="uk-width-1-2@m">
+								<v-autocomplete
+									v-model="filterData.genre"
+									:items="genreTypes"									
+									dense
+									chips
+									label="Gênero"
+								></v-autocomplete>
+							</div>
+							<div class="uk-width-1-4@m">
+								<v-text-field
+									v-model="filterData.yearMin"
+									hint="Digite o ano mínimo"
+									label="Ano (mínimo)"
+									type="number"
+								></v-text-field>
+							</div>
+							<div class="uk-width-1-4@m">
+								<v-text-field
+									v-model="filterData.yearMax"
+									hint="Digite o ano máximo"
+									label="Ano (máximo)"
+									type="number"
+								></v-text-field>
+							</div>
+							<div class="uk-width-1-4@m">
+								<v-text-field
+									v-model="filterData.avgRatingMin"
+									hint="Digite a nota mínima"
+									label="Nota (mínima)"
+									type="number"
+								></v-text-field>
+							</div>
+							<div class="uk-width-1-4@m">
+								<v-text-field
+									v-model="filterData.avgRatingMax"
+									hint="Digite a nota máxima"
+									label="Nota (máxima)"
+									type="number"
+								></v-text-field>
+							</div>
+							<div class="uk-width-1-4@m">
+								<v-text-field
+									v-model="filterData.numVotesMin"
+									hint="Digite os votos mínimos"
+									label="Votos (mínimo)"
+									type="number"
+								></v-text-field>
+							</div>
+							<div class="uk-width-1-4@m">
+								<v-text-field
+									v-model="filterData.numVotesMax"
+									hint="Digite os votos máximos"
+									label="Votos (máxima)"
+									type="number"
+								></v-text-field>
+							</div>
+							<div class="uk-width-1-4@m">
+								<v-text-field
+									v-model="filterData.durationMin"
+									hint="Digite a duração mínima"
+									label="Duração (mínima)"
+									type="number"
+								></v-text-field>
+							</div>
+							<div class="uk-width-1-4@m">
+								<v-text-field
+									v-model="filterData.durationMax"
+									hint="Digite a duração máxima"
+									label="Duração (máxima)"
+									type="number"
+								></v-text-field>
+							</div>
+						</div>
+					</div>
+				</v-card-text>
+                
+				<v-card-actions class="sc-padding-top">
+					<v-spacer></v-spacer>
+					<div>
+						<button class="sc-button sc-button-light sc-button-flat-danger" type="button" @click="dialogFilter = false">
+							Voltar
+						</button>
+					</div>
+					<div>
+						<button class="sc-button sc-button sc-button-danger" type="button" @click="dialogFilter = false">
+							Limpar filtros
+						</button>
+					</div>
+					<div class="" style="padding-left: 3px">
+						<button class="sc-button sc-button-light sc-button-primary"
+							type="button"
+							@click="dialogFilter = false"
+						>
+							Filtrar
+						</button>
+					</div>
+				</v-card-actions>
+			</v-card>
+		</v-dialog>
 	</div>
 </template>
 <script>
@@ -124,11 +263,12 @@ export default {
 					hidden: false,
 					tdClass: 'uk-text-center',
 					thClass: 'uk-text-center',
-					sortable: false,
+					type: 'number',
 				},
 				{
 					label: "Ano",
 					field: "year",
+					type: 'number',
 					hidden: false,
 					thClass: 'text-center',
 					tdClass: 'text-center',
@@ -142,12 +282,20 @@ export default {
 					sortable: false,
 				},
 				{
+					label: "Favoritos",
+					field: "numFavorites",
+					hidden: false,
+					tdClass: 'uk-text-center',
+					thClass: 'uk-text-center',
+					type: 'number',
+				},
+				{
 					label: "Qtd votos",
 					field: "numVotes",
 					hidden: false,
 					tdClass: 'uk-text-right',
 					thClass: 'uk-text-right',
-					sortable: false,
+					type: 'number',
 				},
 				{
 					label: "Média",
@@ -155,7 +303,7 @@ export default {
 					hidden: false,
 					tdClass: 'uk-text-right',
 					thClass: 'uk-text-right',
-					sortable: false,
+					type: 'number',
 				},
 				{
 					label: "",
@@ -199,7 +347,37 @@ export default {
 				genre: '',
 				year: '',
 				country: '',
+				yearMin: null,
+				yearMax: null,
+				durationMin: null,
+				durationMax: null,
+				numVotesMin: null,
+				numVotesMax: null,
+				avgRatingMin: null,
+				avgRatingMax: null,
 			},
+			dialogFilter: false,
+			genreTypes: [
+				{value: "COMEDY", text: "Comédia"},
+				{value: "SCI_FI", text: "Ficção Científica"},
+				{value: "HORROR", text: "Terror"},
+				{value: "ROMANCE", text: "Romance"},
+				{value: "ACTION", text: "Ação"},
+				{value: "THRILLER", text: "Thriller"},
+				{value: "DRAMA", text: "Drama"},
+				{value: "MYSTERY", text: "Mistério"},
+				{value: "CRIME", text: "Crime"},
+				{value: "ANIMATION", text: "Animação"},
+				{value: "ADVENTURE", text: "Aventura"},
+				{value: "FANTASY", text: "Fantasia"},
+				{value: "SUPERHERO", text: "Super-herói"},
+			],
+			movieTypes: [
+				{value: "MOVIE", text: "Filme"},
+				{value: "SHORT", text: "Curta"},
+				{value: "DOCUMENTARY", text: "Documentário"},
+				{value: "VIDEO", text: "Vídeo"},
+			],
 		}
 	},
 	mounted () {
