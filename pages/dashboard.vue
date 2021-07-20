40b79e7d681857d383f8cfcebfb8a24ae7c5330f
gerecorrea <gere.c@hotmail.com>
2021-07-19 21:59:35 -0300

    Melhoria básica dashboard, basicaço

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,41 +1,83 @@
 <template>
 	<div id="sc-page-wrapper">
 		<div id="sc-page-content">
-			HEYYYYY
+			<div class="uk-child-width-1-3@m uk-grid sc-padding uk-margin" style="padding-right: 15px" data-uk-grid>
+				<v-row>
+					<v-col cols="4">
+						<v-card 
+							width="50vh" 
+							height="100px" 
+							class="uk-text-large uk-text-warning uk-text-uppercase uk-text-center sc-padding md-color-white"
+							color="#385F73"
+							@click="redirectPage('/movies/all')"
+						>
+							<img src="~/assets/img/movie_1.png" width="80px" class="sc-padding-right">
+							Filmes
+						</v-card>
+					</v-col>
 
-			<div class="uk-child-width-1-2@m uk-grid sc-padding" data-uk-grid>
-				<v-card 
-					width="50vh" 
-					height="100px" 
-					class="uk-text-large uk-text-warning uk-text-uppercase uk-text-center sc-padding"
-					color="#2AAFC3"
-					@click="redirectPage('/movies/all')"
-				>
-					Listar filmes poarrrr!
-				</v-card>
-				<v-spacer />
+					<v-col cols="4">
+						<v-card 
+							width="50vh" 
+							height="100px" 
+							class="uk-text-large uk-text-warning uk-text-uppercase uk-text-center sc-padding md-color-black"
+							color="#22bbcc"
+							@click="redirectPage('/person/all')"
+						>
+							<img src="~/assets/img/man.png" width="80px" class="sc-padding-right">
+							Estrelas
+						</v-card>
+					</v-col>
+					<v-col cols="4">
+						<v-card 
+							width="50vh" 
+							height="100px" 
+							class="uk-text-large uk-text-warning uk-text-uppercase uk-text-center sc-padding md-color-white"
+							color="green darken-2"
+							@click="redirectPage('/profile/all')"
+						>
+							<img src="~/assets/img/user.png" width="80px" class="sc-padding-right">
+							Usuários da aplicação
+						</v-card>
+					</v-col>
 
-				<v-card 
-					width="50vh" 
-					height="100px" 
-					class="uk-text-large uk-text-warning uk-text-uppercase uk-text-center sc-padding"
-					color="#385F73"
-					@click="redirectPage('/profile/all')"
-				>
-					Listar usuários poarrrr!
-				</v-card>
-				<v-spacer />
+					<v-col cols="4">
+						<v-card 
+							width="50vh" 
+							height="100px" 
+							class="uk-text-large uk-text-warning uk-text-uppercase uk-text-center sc-padding md-bg-deep-orange-a700 md-color-black"
+							color="#2AAFC3"
+							@click="redirectPage('/movies/all')"
+						>
+							<img src="~/assets/img/rating.png" width="80px" class="sc-padding-right">
+							Notas do usuário
+						</v-card>
+					</v-col>
 
-				<v-card 
-					width="50vh" 
-					height="100px" 
-					class="uk-text-large uk-text-warning uk-text-uppercase uk-text-center sc-padding"
-					color="green darken-2"
-					@click="redirectPage('/person/all')"
-				>
-					Listar estrelas poarrrr!
-				</v-card>
-				<v-spacer />
+					<v-col cols="4">
+						<v-card 
+							width="50vh" 
+							height="100px" 
+							class="uk-text-large uk-text-warning uk-text-uppercase uk-text-center sc-padding md-color-black"
+							color="#DDCCDD"
+							@click="redirectPage('/profile/favorites')"
+						>
+							<img src="~/assets/img/movie_2.png" width="80px" class="sc-padding-right">
+							Favoritos do usuário
+						</v-card>
+					</v-col>
+					<v-col cols="4">
+						<v-card 
+							width="50vh" 
+							height="100px" 
+							class="uk-text-large uk-text-warning uk-text-uppercase uk-text-center sc-padding md-bg-deep-purple-400 md-color-white"
+							@click="openTab('http://www.gnccinemas.com.br/index.php/home')"
+						>
+							<img src="~/assets/img/movie-tickets.png" width="80px" class="sc-padding-right">
+							GNC Cinemas
+						</v-card>
+					</v-col>
+				</v-row>
 			</div>
 
 			<div>
@@ -97,6 +139,9 @@ export default {
 			console.log(page)
 			this.$router.push({ path: page });
 		},
+		openTab (url) {
+			window.open(url, '_blank').focus();
+		}
 	}
 }
 </script>
