88ea55a3df43d97d3f16eb5ddf28e286ecf61874
gerecorrea <gere.c@hotmail.com>
2021-07-21 22:14:41 -0300

    Refatoração sidebar e adição de novas navegações

--- components/navigation/sidebar_menu.js
+++ components/navigation/sidebar_menu.js
@@ -5,73 +5,98 @@ export const menuEntries = [
 	{
 		id: uniqueID(),
 		title: "Dashboard",
-		icon: "mdi mdi-view-dashboard md-color-grey-900",
+		icon: "mdi mdi-view-dashboard md-color-green-700",
 		page: "/dashboard"
-	},
-	{
-		id: uniqueID(),
-		title: "Listar filmes",
-		page: "/movies/all",
-		icon: "mdi mdi-movie md-color-grey-900",
-	},
-	{
-		id: uniqueID(),
-		title: "Listar usuários",
-		page: "/profile/all",
-		icon: "mdi mdi-face-profile md-color-brown-500",
-	},
-	{
-		id: uniqueID(),
-		title: "Listar estrelas",
-		page: "/person/all",
-		icon: "mdi mdi-star-circle md-color-yellow-900",
-	},
-	{
-		id: uniqueID(),
-		title: "Avaliações do usuário",
-		page: "/profile/ratings",
-		icon: "mdi mdi-star-half md-color-purple-600"
-	},
-	{
-		id: uniqueID(),
-		title: "Favoritos do usuário",
-		page: "/profile/favorites",
-		icon: "mdi mdi-heart-half-full md-color-red-700"
-	},
-	{
-		id: uniqueID(),
-		title: "Watchlist",
-		page: "/profile/watchlist",
-		icon: "mdi mdi-bookmark-plus-outline md-color-yellow-800"
-	},
-	{
-		id: uniqueID(),
-		title: "Top 10",
-		icon: "mdi mdi-certificate md-color-red-700",
-	},
-	{
-		id: uniqueID(),
-		title: "My ratings",
-		icon: "mdi mdi-script md-color-blue-800"
-	},
-	{
-		id: uniqueID(),
-		title: "Estrelas Favoritas",
-		icon: "mdi mdi-account-star md-color-green-600"
 	},
 	{
 		id: uniqueID(),
 		title: "FILMES",
 		page: "/pages",
-		icon: "mdi mdi-database",
 		isOpen: false,
 		level: 0,
 		submenu: [
 			{
 				id: uniqueID(),
-				title: "Listar todos",
-				icon: "mdi mdi-movie",
+				title: "Top 10 filmes",
+				page: "/movies/topRating",
+				icon: "mdi mdi-movie-roll md-color-teal-400",
+			},
+			{
+				id: uniqueID(),
+				title: "Filmes",
+				page: "/movies/all",
+				icon: "mdi mdi-movie md-color-grey-900",
+			},
+			{
+				id: uniqueID(),
+				title: "Documentários",
+				page: "/movies/allDocumentary",
+				icon: "mdi mdi-camcorder md-color-red-700",
+			},
+			{
+				id: uniqueID(),
+				title: "Curta-metragens",
+				page: "/movies/allShort",
+				icon: "mdi mdi-nature-people md-color-green-700",
 			},
 		]
 	},
+	{
+		id: uniqueID(),
+		title: "USUÁRIO",
+		page: "/pages",
+		isOpen: false,
+		level: 0,
+		submenu: [
+			{
+				id: uniqueID(),
+				title: "Suas avaliações",
+				page: "/profile/ratings",
+				icon: "mdi mdi-star-half md-color-purple-600"
+			},
+			{
+				id: uniqueID(),
+				title: "Seus favoritos",
+				page: "/profile/favorites",
+				icon: "mdi mdi-heart-half-full md-color-red-700"
+			},
+			{
+				id: uniqueID(),
+				title: "Sua watchlist",
+				page: "/profile/watchlist",
+				icon: "mdi mdi-bookmark-plus-outline md-color-yellow-800"
+			},
+		]
+	},
+	{
+		id: uniqueID(),
+		title: "ARTISTAS",
+		page: "/pages",
+		isOpen: false,
+		level: 0,
+		submenu: [
+			{
+				id: uniqueID(),
+				title: "Listar artistas",
+				page: "/person/all",
+				icon: "mdi mdi-star-circle md-color-yellow-900",
+			},
+		]
+	},
+	{
+		id: uniqueID(),
+		title: "CONFIGURAÇÃO",
+		page: "/pages",
+		isOpen: false,
+		level: 0,
+		submenu: [
+			{
+				id: uniqueID(),
+				title: "Listar usuários",
+				page: "/profile/all",
+				icon: "mdi mdi-face-profile md-color-brown-500",
+			},
+		]
+	},
+
 ];
