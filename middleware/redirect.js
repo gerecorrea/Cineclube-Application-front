6d098c66d62240fababd3df3afe5729858696ac3
gerecorrea <gere.c@hotmail.com>
2021-11-17 22:11:28 -0300

    Criação de redirect genérico básico para uso futuro ao logar na app.

--- middleware/redirect.js
+++ middleware/redirect.js
@@ -1,5 +1,5 @@
 export default function ({ route, redirect }) {
 	if (route.path === '/') {
-		redirect('/dashboard')
+		redirect('/redirect')
 	}
 }
---
+++ pages/redirect.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="mdi mdi-48px mdi-spin">
+	</div>
+</template>
+
+<script>
+
+export default {
+	mounted () {
+		this.redirectPage("/dashboard");
+	},
+	methods: {
+		redirectPage (page) {
+			this.$router.push({ path: page });
+		},
+	},
+}
+</script>
